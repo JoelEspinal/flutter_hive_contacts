627398de5adcafedbed41d771b6b6e4f7fefbfe9
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-08-07 10:05:18 -0400

    🚧 Add recipe class

---
+++ lib/src/models/recipe.dart
@@ -0,0 +1,8 @@
+class Recipe {
+  String name;
+  String comercialName;
+  String imagePath;
+  List<String> ingredients;
+
+  Recipe(this.name, this.comercialName, this.imagePath, this.ingredients);
+}
